b5ee572512fd9ebdaa432a1d342778adb8c60b9f
Eric <ericshuanders@gmail.com>
2020-12-18 14:54:56 -0500

    Added landing arrow and adjusted fonts throughout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './App.css';
 import {Landing} from './pages/landing';
 import {About} from './pages/about';
 import {Projects} from './pages/projects';
 import {Contact} from './pages/contact'
 function App() {
+
+  const aboutRef = useRef()
+  const handleClick=()=>{
+    aboutRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
   return (
     <div className="App">
-     <Landing/>
-     <About/>
+     
+     <Landing handleClick={handleClick}/>
+     <About ref={aboutRef}/>
      <Projects/>
      <Contact/>
+    
     </div>
   );
 }
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import {Fade} from 'react-reveal'
 import './Landing.css'
-const Landing = () => {
+import Arrow from './assets/next.png'
+const Landing = ({handleClick}) => {
+  
     return (
      
-        <div className="container">
+        <section className="container">
+          <div className="layers">
+          <div className="base">
           <Fade top>
           <h1>Hi, I'm Eric </h1>
+          
           </Fade>
-       
-        </div>
+          </div>
+         
+          <div className="overlay">
+            <h2>I'm a Software Engineer</h2>
+            <img src={Arrow} alt="downward-arrow" onClick={handleClick}/>
+          </div>
+          
+          </div>
+        </section>
 
         
     )
